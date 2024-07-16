5e43152c5d5901f05b08b755e2efa5aa50dda4ea
ValerioL96 <v.lembo1396@gmail.com>
2024-07-16 08:55:12 +0200

    Added header and img

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,15 @@
 <header>
-
+    <section class="container-header">
+        <nav>
+            <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="logo DC">
+        </nav>
+        <nav>
+            <ul>
+                @foreach ($links as $link )
+                <li class="{{$link['active'] ? 'active' : ''}}">
+                    {{$link['name']}}
+                </li>
+            </ul>
+        </nav>
+    </section>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,58 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $links = [
+        [
+            "id" => 1,
+            "name" => 'Characters',
+            "active" => false,
+        ],
+        [
+            "id" => 2,
+            "name" => 'comics',
+            "active" => true,
+        ],
+        [
+            "id" => 3,
+            "name" => 'movies',
+            "active" => false,
+        ],
+        [
+            "id" => 4,
+            "name" => 'tv',
+            "active" => false,
+        ],
+        [
+            "id" => 5,
+            "name" => 'games',
+            "active" => false,
+        ],
+        [
+            "id" => 6,
+            "name" => 'collectibles',
+            "active" => false,
+        ],
+        [
+            "id" => 7,
+            "name" => 'videos',
+            "active" => false,
+        ],
+        [
+            "id" => 8,
+            "name" => 'fans',
+            "active" => false,
+        ],
+        [
+            "id" => 9,
+            "name" => 'news',
+            "active" => false,
+        ],
+        [
+            "id" => 10,
+            "name" => 'shop',
+            "active" => false,
+        ],
+    ];
     return view('pages.home');
 });
